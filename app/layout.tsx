9e5af25ef91e3b95c66c90969600feb09f3516f9
Belrick Stephane <73307766+latioms@users.noreply.github.com>
2025-08-25 15:22:39 +0100

    refactor: simplify layout structure and remove unnecessary providers
    - Removed SystemProvider and Sidebar from RootLayout, directly rendering children.
    - Updated Toaster component to include theme handling and simplified toast options.
    - Removed unused formatDate function from utils.
    - Reorganized package.json dependencies, moving some to the correct position.
    - Updated pnpm-lock.yaml to reflect changes in package dependencies.
    - Added components.json configuration for shadcn UI components.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import SystemProvider from "@/components/providers/SystemProvider";
-import { Sidebar } from "@/components/providers/SidebarProvider";
 import { Toaster } from "sonner";
 
 const geistSans = Geist({
@@ -29,9 +27,7 @@ export default function RootLayout({
     <html lang="fr">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Toaster richColors />
-        <SystemProvider>
-          <Sidebar>{children}</Sidebar>
-        </SystemProvider>
+          {children}
       </body>
     </html>
   );
