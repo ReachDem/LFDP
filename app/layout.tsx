ea4d509686f91f2ab54a7f1b4ee78d5a2cd4c3e6
Nickgiresse <kamdemnick12@gmail.com>
2025-08-27 14:42:04 +0200

    fix: add dashbord

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
 
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Toaster richColors />
           {children}
+           
       </body>
     </html>
   );
